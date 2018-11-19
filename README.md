a415a655924e5e897312bc36444f07c1a2e846d2
Bulat Mendagaziev <mendagazievb@gmail.com>
2018-11-19 18:15:15 +0400

    added solution for first task #time 2h

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # code-challenge
+
+1. Реализация функции curry `curry.js`, было выполнено два варианта через функцию и метод.
+
+```javascript
+function abc(a, b, c) {
+  return a + b + c;
+}
+
+function abcdef(a, b, c, d, e, f) {
+  return a + b + c + d + e + f;
+}
+
+// Первый вариант вызова через функцию
+
+curry(abc)('A')('B')('C'); // 'ABC'
+curry(abc)('A', 'B')('C'); // 'ABC'
+curry(abc)('A', 'B', 'C'); // 'ABC'
+
+curry('A')('B')('C')('D')('E')('F')(abcdef);
+curry('A', 'B', 'C')('D', 'E', 'F')(abcdef);
+
+// Второй вариант через метод
+
+abc.curry('A')('B')('C'); // 'ABC'
+// ...
+abcdef.curry('A', 'B', 'C')('D', 'E', 'F'); // 'ABCDEF'
+```
---
+++ curry.js
@@ -0,0 +1,71 @@
+/**
+ * Первый вариант реализации функции каррирования
+ *
+ * @return {function}
+ */
+const curry = function () {
+  let _args = Array.prototype.slice.call(arguments);
+  let _callback = this._callback || null;
+  let _filterArgs = this._filterArgs || [];
+
+  _args.forEach(arg => {
+    if (typeof arg === 'function') {
+      _callback = arg;
+    } else {
+      _filterArgs.push(arg);
+    }
+  });
+
+  if (_callback && _callback.length ===  _filterArgs.length) {
+    return _callback.apply(null, _filterArgs)
+  } else {
+    return curry.bind({_callback, _filterArgs})
+  }
+};
+
+const assert = (a, b) => {
+  if (a !== b) {
+    throw new Error(`value ${a} is not equal to the ${b}`);
+  }
+};
+
+const abc = function (a, b, c) {
+  return a + b + c;
+};
+
+const abcdef = function (a, b, c, d, e, f) {
+  return a + b + c + d + e + f;
+};
+
+assert(curry(abc)('A')('B')('C'), 'ABC');
+assert(curry(abc)('A', 'B')('C'), 'ABC');
+assert(curry(abc)('A', 'B', 'C'), 'ABC');
+
+assert(curry('A')('B')('C')('D')('E')('F')(abcdef), 'ABCDEF');
+assert(curry('A', 'B', 'C')('D', 'E', 'F')(abcdef), 'ABCDEF');
+
+/**
+ * Второй вариант реализации из примера в readme abc.curry('A')('B')('C');
+ * Изменяется стандартный прототип, думаю это не самая удачная идея
+ * */
+
+Function.prototype.curry = function () {
+  let _callback = this._callback || this;
+  let _args = Array.prototype.slice.call(arguments);
+  let _filterArgs = this._filterArgs ? [...this._filterArgs, ..._args] : _args;
+
+  if (_callback.length ===  _filterArgs.length) {
+    return _callback.apply(null, _filterArgs)
+  } else {
+    return Function.prototype.curry.bind({_callback, _filterArgs})
+  }
+
+};
+
+assert(abc.curry('A')('B')('C'), 'ABC');
+assert(abc.curry('A', 'B')('C'), 'ABC');
+assert(abc.curry('A', 'B', 'C'), 'ABC');
+
+assert(abcdef.curry('A')('B')('C')('D')('E')('F'), 'ABCDEF');
+assert(abcdef.curry('A', 'B', 'C')('D', 'E', 'F'), 'ABCDEF');
+
